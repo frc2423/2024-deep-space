f65d12955bfabd105748bad4f3151c279e1ae332
Ben Skraly <benskraly@gmail.com>
2024-11-19 20:49:03 -0500

    goUp and goDwon down and periodic function done

--- DeepSpace/src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
+++ DeepSpace/src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
@@ -1,4 +1,6 @@
+import com.revrobotics.CANSparkFlex;
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkLowLevel.MotorType;
 
 import edu.wpi.first.math.controller.ElevatorFeedforward;
 import edu.wpi.first.math.controller.ProfiledPIDController;
@@ -11,18 +13,34 @@ import edu.wpi.first.math.geometry.Translation2d;
 
 public class ElevatorSubsystem extends SubsystemBase {
     ProfiledPIDController elevator_PID = new ProfiledPIDController(0, 0, 0, new TrapezoidProfile.Constraints(0, 0));// noice
-    private Translation2d elevatorPose = new Translation2d(0, 0);
-    private Translation2d setpoint = new Translation2d(0, 0);
+    ProfiledPIDController elevator_PID_Manual = new ProfiledPIDController(0, 0, 0, new TrapezoidProfile.Constraints(0, 0));// for the goUp and goDown for manual (slower maxVel)
+    private double elevatorCurrentPose = 0;
+    private double setpoint = 0;
     private final ElevatorFeedforward m_feedforward = new ElevatorFeedforward(0, 0, 0, 0);
-    private CANSparkMax motor;
+    private CANSparkFlex motor1 = new CANSparkFlex(24 , MotorType.kBrushless);
+    private CANSparkFlex motor2 = new CANSparkFlex(26 , MotorType.kBrushless);
+    private double highestPoint = 5.4;
+    private double lowestPoint = 0;
+
+
 
     public ElevatorSubsystem() {
+        
+    }
+
+    @Override
+    public void periodic() {
+        double calculatedPID = calculatePid(setpoint);
+        motor1.set(calculatedPID);
+        motor2.set(-calculatedPID); //ONE OF THEM IS NEGITIVE
+
+      
 
     }
 
-    private double calculatePid(Translation2d position) {
+    private double calculatePid(double position) {
         // updatePivotAngle();
-        double pid = elevator_PID.calculate(elevatorPose.getY(), position.getY());
+        double pid = elevator_PID.calculate(elevatorCurrentPose, position);
         var setpoint = elevator_PID.getSetpoint();
         double feedforward = m_feedforward.calculate(0, 0);
         // return feedforward + pid;
@@ -30,19 +48,21 @@ public class ElevatorSubsystem extends SubsystemBase {
     }
 
     public void goUp() { // for manual control
+        setpoint = highestPoint;
+
 
     }
 
     public void goDown() { // for manual control
-
+        setpoint = lowestPoint;
     }
 
     public void stopElevator() {
-
+        setpoint = elevatorCurrentPose;
     }
 
     public double getElevatorVelocity() {
-        return motor.getEncoder().getVelocity();
+        return motor1.getEncoder().getVelocity();
     }
 
 }
